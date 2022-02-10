e5d6f3860c5fea9108c0ce8210f6d041ee9243e6
Zerg93 <41504334+yoonmyungseok@users.noreply.github.com>
2022-02-10 10:51:58 +0900

    Create 두 개 뽑아서 더하기.java

---
+++ java/level1/두 개 뽑아서 더하기.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+import java.util.Set;
+class Solution {
+    public Set<Integer> solution(int[] numbers) {
+        Set<Integer> answer=new TreeSet<>();
+        for(int i=0;i<numbers.length;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                answer.add(numbers[i]+numbers[j]);
+            }
+        }
+        return answer;
+    }
+}
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
