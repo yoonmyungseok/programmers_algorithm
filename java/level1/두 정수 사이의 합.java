7af27d0934e7558dc0c29d473285cbf077b5c2b0
Zerg93 <41504334+yoonmyungseok@users.noreply.github.com>
2022-02-12 13:18:57 +0900

    Create 두 정수 사이의 합.java

---
+++ java/level1/두 정수 사이의 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+        for(int i = min ; i<=max; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
