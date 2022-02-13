3b87057dcf8f5a50118add01779dafcf3c8914ca
Zerg93 <41504334+yoonmyungseok@users.noreply.github.com>
2022-02-13 10:31:40 +0900

    Create 하샤드 수.java

---
+++ java/level1/하샤드 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(int x) {
+        int n=x;
+        int sum=0;
+        while(n>0){
+            sum+=n%10;
+            n/=10;
+        }
+        return x%sum==0 ? true : false;  
+    }
+}
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
