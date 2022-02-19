82670e50fe1f64f0746ead7ff913a66731f7444a
Zerg93 <41504334+yoonmyungseok@users.noreply.github.com>
2022-02-19 17:10:26 +0900

    Create 문자열 다루기 기본.java

---
+++ java/level1/문자열 다루기 기본.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        for(int i=0;i<s.length();i++){
+            if((s.charAt(i)>=48&&s.charAt(i)<=57)&&(s.length()==4||s.length()==6)){
+                answer=true;
+            }else{
+                answer=false;
+                break;
+            }  
+        }
+        return answer;
+    }
+}
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
