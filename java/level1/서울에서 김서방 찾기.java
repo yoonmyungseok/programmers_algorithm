8f20850d54b0ed96919a3c2aec550fb231e1f755
Zerg93 <41504334+yoonmyungseok@users.noreply.github.com>
2022-02-19 17:11:06 +0900

    Create 서울에서 김서방 찾기.java

---
+++ java/level1/서울에서 김서방 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        String name="Kim";
+        for(int i=0;i<seoul.length;i++){
+            if(seoul[i].equals(name)){
+                answer="김서방은 "+i+"에 있다"; 
+                break;
+            }
+        }
+        return answer;
+    }
+}
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
