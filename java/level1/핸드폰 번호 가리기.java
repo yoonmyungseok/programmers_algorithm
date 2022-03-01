bdaeeb4718dce0f0dd0434f3aa651faa61152fad
Zerg93 <41504334+yoonmyungseok@users.noreply.github.com>
2022-03-01 12:27:14 +0900

    Create 핸드폰 번호 가리기.java

---
+++ java/level1/핸드폰 번호 가리기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        char[] ch=new char[phone_number.length()];
+        for(int i=0;i<ch.length;i++){
+            if(i<ch.length-4){
+                ch[i]='*';
+            }else{
+                ch[i]=phone_number.charAt(i);
+            }
+        }
+        answer=String.valueOf(ch);
+        return answer;
+    }
+}
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
