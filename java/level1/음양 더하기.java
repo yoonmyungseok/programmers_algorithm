9ec472a7a22427a672d5f9598c4cfd1cb3b99e04
Zerg93 <41504334+yoonmyungseok@users.noreply.github.com>
2022-03-06 21:21:58 +0900

    Create 음양 더하기.java

---
+++ java/level1/음양 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum = 0;
+        for(int i=0;i<absolutes.length;i++){
+            if(signs[i]==true){
+                sum+=absolutes[i];
+            }else{
+                sum+=(absolutes[i]*-1);
+            }
+        }
+        return sum;
+    }
+}
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
