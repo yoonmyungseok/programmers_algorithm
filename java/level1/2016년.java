3865eb9999b0353c256eca59c9eb02d342b400f8
Zerg93 <41504334+yoonmyungseok@users.noreply.github.com>
2022-03-23 10:06:22 +0900

    Create 2016년.java

--- java/level1/2016년.java
+++ java/level1/2016년.java
@@ -1,13 +1,7 @@
+import java.time.*;
 class Solution {
-    public String solution(int a, int b) {
-        String[] week={"FRI","SAT","SUN","MON","TUE","WED","THU"};
-        int[] day={31,29,31,30,31,30,31,31,30,31,30,31};
-        int sum=0;
-        for(int i=0;i<a-1;i++){
-            sum+=day[i];
-        }
-        int date=(sum+b-1)%7;
-        return week[date];
-    }
+  public String solution(int a, int b) {
+      return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);
+  }
 }
 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
