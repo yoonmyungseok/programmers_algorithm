862b073dca51925585643c858a198d8df0bcce6e
yoonmyungseok <yoonms5319@gmail.com>
2022-03-25 16:24:25 +0900

    0325

--- java/level1/직사각형 별찍기.java
+++ java/level1/직사각형 별찍기.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                System.out.print("*");
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            for(int i=0; i<m; i++){
+                for(int j=0; j<n; j++){
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
